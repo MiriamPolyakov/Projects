0192aff84c653b972a9c969ad93269efc7c906d7
MiriamPolyakov <144250052+MiriamPolyakov@users.noreply.github.com>
2023-12-28 11:44:07 +0200

    Update chatApp.py

--- DevSecOps/Chat-App-Project/chatApp.py
+++ DevSecOps/Chat-App-Project/chatApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, request, session
 import csv
 import os
 # import requests
